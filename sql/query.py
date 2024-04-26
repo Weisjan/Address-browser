06ad423a79d9c03a2873270079fceacca31b076c
Weisjan <106084713+Weisjan@users.noreply.github.com>
2024-04-26 13:26:56 +0200

    Update query.py

--- sql/query.py
+++ sql/query.py
@@ -1,3 +1,129 @@
+SEKWENCJE = '''
+            CREATE SEQUENCE IF NOT EXISTS panstwo_id_seq_;
+            CREATE SEQUENCE IF NOT EXISTS wojewodztwo_id_seq_;
+            CREATE SEQUENCE IF NOT EXISTS powiat_id_seq_;
+            CREATE SEQUENCE IF NOT EXISTS gmina_id_seq_;
+            CREATE SEQUENCE IF NOT EXISTS miejscowosc_id_seq_;
+            CREATE SEQUENCE IF NOT EXISTS ulica_id_seq_;
+            CREATE SEQUENCE IF NOT EXISTS adres_id_seq_;
+            '''
+
+TWORZENIE_PANSTWA = '''
+            CREATE TABLE IF NOT EXISTS public.panstwo_ (
+            id_panstwa serial,
+            nazwa text,
+            CONSTRAINT id_panstwa__pk PRIMARY KEY (id_panstwa),
+            CONSTRAINT panstwo__nazwa UNIQUE (nazwa));
+            '''
+
+TWORZENIE_WOJEWODZTWA = '''
+            CREATE TABLE IF NOT EXISTS public.wojewodztwo_ (
+            id_wojewodztwa serial,
+            nazwa text,
+            teryt text,
+            id_panstwa integer,
+            CONSTRAINT id_wojewodztwa__pk PRIMARY KEY (id_wojewodztwa),
+            CONSTRAINT woj__teryt_uni UNIQUE (teryt),
+            CONSTRAINT id_panstwa__fk FOREIGN KEY (id_panstwa)
+            REFERENCES public.panstwo_ (id_panstwa) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+            NOT VALID);
+            '''
+
+TWORZENIE_POWIATU = '''
+            CREATE TABLE IF NOT EXISTS public.powiat_ (
+            id_powiatu serial,
+            nazwa text,
+            teryt text,
+            id_wojewodztwa integer,
+            CONSTRAINT id_powiatu__pk PRIMARY KEY (id_powiatu),
+            CONSTRAINT pow__teryt_uni UNIQUE (teryt),
+            CONSTRAINT id_wojewodztwa__fk FOREIGN KEY (id_wojewodztwa)
+            REFERENCES public.wojewodztwo_ (id_wojewodztwa) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+            NOT VALID);
+            '''
+
+TWORZENIE_GMINY = '''
+            CREATE TABLE IF NOT EXISTS public.gmina_ (
+            id_gminy serial,
+            nazwa text,
+            teryt text,
+            id_powiatu integer,
+            CONSTRAINT id_gminy__pk PRIMARY KEY (id_gminy),
+            CONSTRAINT gm__teryt_uni UNIQUE (teryt),
+            CONSTRAINT id_powiatu__fk FOREIGN KEY (id_powiatu)
+            REFERENCES public.powiat_ (id_powiatu) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+            NOT VALID);
+            '''
+
+TWORZENIE_MIEJSCOWOSCI = '''
+            CREATE TABLE IF NOT EXISTS public.miejscowosc_ (
+            id_miejscowosci serial,
+            nazwa text,
+            rodzaj text,
+            teryt text,
+            id_gminy integer,
+            miejsciipid text,
+            CONSTRAINT id_miejscowosci__pk PRIMARY KEY (id_miejscowosci),
+            CONSTRAINT miejsc__miejsciipid_uni UNIQUE (miejsciipid),
+            CONSTRAINT id_gminy__fk FOREIGN KEY (id_gminy)
+            REFERENCES public.gmina_ (id_gminy) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+            NOT VALID);
+            '''
+
+TWORZENIE_ULICY = '''
+            CREATE TABLE IF NOT EXISTS public.ulica_ (
+            id_ulicy serial,
+            nazwa text,
+            nazwa_czesc text,
+            nazwa_przed1 text,
+            nazwa_przed2 text,
+            typ text,
+            teryt text,
+            id_miejscowosci integer,
+            uliipid text,
+            CONSTRAINT id_ulicy__pk PRIMARY KEY (id_ulicy),
+            CONSTRAINT ul__uliipid UNIQUE (uliipid),
+            CONSTRAINT id_miejscowosci__fk FOREIGN KEY (id_miejscowosci)
+            REFERENCES public.miejscowosc_ (id_miejscowosci) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+            NOT VALID);
+            '''
+
+TWORZENIE_ADRESU = '''
+            CREATE TABLE IF NOT EXISTS public.adres_ (
+            id_adresu serial,
+            kod_pocztowy text,
+            numer text,
+            status text,
+            lat text,
+            lon text,
+            id_ulicy integer,
+            id_miejscowosci integer,
+            pktprgiipid text,
+            CONSTRAINT id_adresu__pk PRIMARY KEY (id_adresu),
+            CONSTRAINT adr__pktprgiipid UNIQUE (pktprgiipid),
+            CONSTRAINT id_ulicy__fk FOREIGN KEY (id_ulicy)
+            REFERENCES public.ulica_ (id_ulicy) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+            NOT VALID,
+            CONSTRAINT id_miejscowosci__fk FOREIGN KEY (id_miejscowosci)
+            REFERENCES public.miejscowosc_ (id_miejscowosci) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+            NOT VALID);
+            '''
+
+
 PANSTWO = '''
             INSERT INTO public.panstwo (id_panstwa, nazwa)
             VALUES (nextval('panstwo_id_seq'), %s);
